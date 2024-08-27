47077104bde3dae72c5c49e08e046aae34d35105
ErlendKK <136587757+ErlendKK@users.noreply.github.com>
2024-08-28 01:35:15 +0200

    Oppdatert for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Plantegning-m-ler/",
   optimizeDeps: {
     include: ["pdfjs-dist"],
   },
